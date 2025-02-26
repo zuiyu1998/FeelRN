48740e9a3baa5a5dfc16248d0f29948744baa723
zuiyu1998 <1542844298@qq.com>
2025-02-26 23:44:12 +0800

    feat: 添加alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
